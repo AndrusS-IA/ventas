2e2bc99c404621a9877a1ec6990bea2a8144ac32
Andrus Ariel Sanchez <negro_catracho@hotmail.com>
2019-07-08 19:10:53 -0600

    Ultimos Cambios en Frase Motivadora

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -17,7 +17,7 @@ namespace Ventas
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
             MessageBox.Show("Siempre adelante, para atras ni para tomar impulso");
         }
